786fe1de5b09ea4ab9bd363337037a9ce6c0ae7c
die <dean.s.yangdemon@gmail.com>
2024-09-24 22:02:49 +0800

    frsat:ddd

--- php/check_user.php
+++
@@ -1,5 +0,0 @@
-<?php
-header("Access-Control-Allow-Origin:*");
-require_once("data_config.php");
-echo check_user_exist($_POST["phone"]);
-?>
--- php/new_user.php
+++
@@ -1,4 +0,0 @@
-<?php
-require_once("data_config.php");
-echo insert_new_user("未命名",$_POST["phone"],$_POST["pw"])
-?>
